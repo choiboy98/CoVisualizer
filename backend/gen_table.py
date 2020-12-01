8487d4a0cf63c46bd4bad67fd14d91aacc1490b7
choiboy98 <daniel.won.choi@gmail.com>
2020-12-01 00:48:55 -0800

    deploying

--- backend/gen_table.py
+++ backend/gen_table.py
@@ -1,5 +1,19 @@
 import psycopg2
-from config import config
+from sqlalchemy import *
+from config import host, port, database, user, password
+
+conn_str = f"postgresql://{user}:{password}@{host}/{database}"
+engine = create_engine(conn_str)
+connection = engine.connect()
+metadata = MetaData()
+first_tb = Table('first_table', metadata,
+   Column('id', Integer, primary_key=True),
+   Column('name', String(255), nullable=False),
+   Column('isHappy', Boolean, nullable=False)
+)
+metadata.create_all(engine)
+query = insert(first_tb).values(id=1, name="Student", isHappy=True)
+ResultProxy = connection.execute(query)
 
 
 def create_tables():
