4d515e89b0861d04ed3d3cd72eb6c53e9309fd2d
Thomas Yang <thomasy4@illinois.edu>
2020-12-02 21:20:54 -0500

    fixing netid null issue

--- frontend/utility/ApiConfig.js
+++ frontend/utility/ApiConfig.js
@@ -1,6 +1,6 @@
 let BACKEND_URL, MONGO_URL
 
-BACKEND_URL = 'http://c51381b67022.ngrok.io'
+BACKEND_URL = 'http://f98b60d36ba2.ngrok.io'
 MONGO_URL = 'http://127.0.0.1:4000/api/'
 
 export { BACKEND_URL, MONGO_URL }
